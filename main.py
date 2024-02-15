3f91b46b3ae8d98030da09453424864382dc73db
Jeanvk <jeanvecko97@gmail.com>
2024-02-15 15:45:57 +0100

    Firstcommit

--- main.py
+++ main.py
@@ -9,3 +9,6 @@ application_name = "nom_de_votre_application"
 environment_name = "nom_de_votre_environnement"
 chemin_application = "/chemin/vers/votre/application"
 nom_version = "v1.0"  # Par exemple, vous pouvez utiliser un numéro de version ou un tag git
+# Créer une connexion vers les services AWS
+eb_client = boto3.client('elasticbeanstalk')
+s3_client = boto3.client('s3')
