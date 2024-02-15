2a5f0b3c5ef80e8bf1d370faf08b2189238b2a71
Jeanvk <jeanvecko97@gmail.com>
2024-02-15 10:39:09 +0100

    Firstcommit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Configuration des paramètres
+application_name = "nom_de_votre_application"
+environment_name = "nom_de_votre_environnement"
+chemin_application = "/chemin/vers/votre/application"
+# Importation des modules Boto3 et d'autres modules nécessaires
+import boto3
+# Configuration des paramètres
+application_name = "nom_de_votre_application"
+environment_name = "nom_de_votre_environnement"
+chemin_application = "/chemin/vers/votre/application"
+nom_version = "v1.0"  # Par exemple, vous pouvez utiliser un numéro de version ou un tag git
